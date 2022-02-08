55d589e392a1a4afb1c45a318ecab85f98dce7a2
William Ho <yunwaiho@gmail.com>
2022-02-08 21:09:01 +1100

    cplex research

---
+++ code-team-selection/selection.py
@@ -0,0 +1,17 @@
+
+
+class Player:
+    # To track: whether players are played at a time point t
+    
+    def __init__(self, player_id, age, position, market_value, player_salary, is_raritan) -> None:
+        
+        player_id = player_id
+        age_0 = age
+        position = position
+
+        # (simulation x year) array
+        value = market_value
+        salary = player_salary
+        
+        is_raritan = is_raritan
+
